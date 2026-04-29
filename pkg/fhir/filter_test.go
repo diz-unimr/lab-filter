088c417dfb4ba748471dbff6e4b1866904fe61b3
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2026-04-29 23:28:17 +0200

    feat: Set FHIR profile (#4)
    * fix: truncating report result after filter
    
    * ci: apply De Morgan's law
    
    * ci: run build action only once on pull requests

--- pkg/fhir/filter_test.go
+++ pkg/fhir/filter_test.go
@@ -1,10 +1,15 @@
 package fhir
 
 import (
-	"github.com/samply/golang-fhir-models/fhir-models/fhir"
-	"golang.org/x/exp/slices"
+	"encoding/json"
+	"lab-filter/pkg/config"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	//"github.com/magiconair/properties/assert"
+	"github.com/samply/golang-fhir-models/fhir-models/fhir"
 )
 
 func TestFilterBundle(t *testing.T) {
@@ -52,7 +57,11 @@ func TestFilterBundle(t *testing.T) {
 `)
 
 	// act
-	result := FilterBundle(testBundle)
+	result := NewLabFilter(config.Fhir{Profiles: config.Profiles{
+		ServiceRequest:   nil,
+		DiagnosticReport: nil,
+		Observation:      nil,
+	}}).FilterBundle(testBundle)
 
 	// assert
 	expected := []string{"report", "quantity-obs"}
@@ -71,7 +80,83 @@ func TestFilterBundle(t *testing.T) {
 		actual = append(actual, *res.Id)
 	}
 
-	if !slices.Equal(actual, expected) {
-		t.Errorf("Expected bundle to contain entry ids (%s) but was (%s)", strings.Join(expected, ","), strings.Join(actual, ","))
+	assert.ElementsMatchf(t, actual, expected, "Expected bundle to contain entry ids (%s) but was (%s)",
+		strings.Join(expected, ","), strings.Join(actual, ","))
+}
+
+func TestFilterSetsProfiles(t *testing.T) {
+	// test bundle
+	testBundle := []byte(`
+{
+  "resourceType": "Bundle",
+  "type": "batch",
+  "entry": [
+    {
+      "resource": {
+        "resourceType": "DiagnosticReport",
+        "id": "report",
+        "result": [
+          {
+            "reference": "Observation/quantity-obs"
+          }
+        ]
+      }
+    },
+    {
+      "resource": {
+        "resourceType": "Observation",
+        "id": "quantity-obs",
+        "valueQuantity": {
+          "value": 24,
+          "unit": "µg/l",
+          "system": "http://unitsofmeasure.org",
+          "code": "ng/mL"
+        }
+      }
+    },
+    {
+      "resource": {
+        "resourceType": "ServiceRequest",
+        "id": "request",
+        "meta": {
+          "source": "#swisslab",
+          "tag": [
+            {
+              "system": "https://fhir.diz.uni-marburg.de/CodeSystem/mapper",
+              "code": "hl7-lab-mapper"
+            }
+          ]
+        }
+      }
+    }
+  ]
+}
+`)
+	profiles := config.Fhir{Profiles: config.Profiles{
+		ServiceRequest:   ref("https://www.medizininformatik-initiative.de/fhir/core/modul-labor/StructureDefinition/ServiceRequestLab|2026.0.0"),
+		DiagnosticReport: ref("https://www.medizininformatik-initiative.de/fhir/core/modul-labor/StructureDefinition/DiagnosticReportLab|2026.0.0"),
+		Observation:      ref("https://www.medizininformatik-initiative.de/fhir/core/modul-labor/StructureDefinition/ObservationLab|2026.0.0"),
+	}}
+
+	// act
+	result := NewLabFilter(profiles).FilterBundle(testBundle)
+
+	// assert
+	for _, e := range result.Entry {
+		res, _ := fhir.UnmarshalResource(e.Resource)
+		var dto ResourceTypeDto
+		_ = json.Unmarshal(e.Resource, &dto)
+		switch *dto.Type {
+		case "ServiceRequest":
+			assert.Equal(t, res.Meta.Profile, []string{*profiles.Profiles.ServiceRequest})
+		case "DiagnosticReport":
+			assert.Equal(t, res.Meta.Profile, []string{*profiles.Profiles.DiagnosticReport})
+		case "Observation":
+			assert.Equal(t, res.Meta.Profile, []string{*profiles.Profiles.Observation})
+		}
 	}
+}
+
+func ref(str string) *string {
+	return &str
 }
